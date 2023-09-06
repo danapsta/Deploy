3ce67451d9568ed3801b23845c264d9e809856ad
DaNapsta <bayates826@gmail.com>
2023-09-06 15:40:35 -0500

    modified:   Applications/Menu.bat

--- Applications/Menu.bat
+++ Applications/Menu.bat
@@ -4,12 +4,12 @@ del %cpath%\variables.bat
 
 REM Unblock Powershell Script files from prompting for execution
 
-powershell.exe Unblock-File -Path "%cpath%\Menu.ps1"
-powershell.exe Unblock-File -Path "%cpath%\Default\Configure Workstation.ps1"
-powershell.exe Unblock-File -Path "%cpath%\Default\Install Windows 10 Feature Update.ps1"
-powershell.exe Unblock-File -Path "%cpath%\Default\Install Windows Patches.ps1"
-powershell.exe Unblock-File -Path "%cpath%\BGInfo_Automated_Windows_Server_2012_R2.ps1"
-powershell.exe Unblock-File -Path "%cpath%\Default\Launcher.ps1"
+powershell.exe Unblock-File -Path '%cpath%\Menu.ps1'
+powershell.exe Unblock-File -Path '%cpath%\Default\Configure Workstation.ps1'
+powershell.exe Unblock-File -Path '%cpath%\Default\Install Windows 10 Feature Update.ps1'
+powershell.exe Unblock-File -Path '%cpath%\Default\Install Windows Patches.ps1'
+powershell.exe Unblock-File -Path '%cpath%\Default\BGInfo_Automated_Windows_Server_2012_R2.ps1'
+powershell.exe Unblock-File -Path '%cpath%\Default\Launcher.ps1'
 
 REM Run the Deployment Tool
 
