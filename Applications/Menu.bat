b37f565fcdcb6cb1a30ca3ae1ca0423c19a9732b
DaNapsta <bayates826@gmail.com>
2023-09-06 15:33:38 -0500

    modified:   Applications/Menu.bat

--- Applications/Menu.bat
+++ Applications/Menu.bat
@@ -2,5 +2,16 @@ set cpath=C:\Users\%username%\Desktop\Applications
 
 del %cpath%\variables.bat
 
-powershell.exe -executionpolicy unrestricted -File "%cpath%\Menu.ps1" -Verb RunAs
+REM Unblock Powershell Script files from prompting for execution
+
+powershell.exe Unblock-File -Path "%cpath%\Menu.ps1"
+powershell.exe Unblock-File -Path "%cpath%\Default\Configure Workstation.ps1"
+powershell.exe Unblock-File -Path "%cpath%\Default\Install Windows 10 Feature Update.ps1"
+powershell.exe Unblock-File -Path "%cpath%\Default\Install Windows Patches.ps1"
+powershell.exe Unblock-File -Path "%cpath%\BGInfo_Automated_Windows_Server_2012_R2.ps1"
+powershell.exe Unblock-File -Path "%cpath%\Default\Launcher.ps1"
+
+REM Run the Deployment Tool
+
+powershell.exe -executionpolicy Bypass -File "%cpath%\Menu.ps1" -Verb RunAs
 PAUSE
